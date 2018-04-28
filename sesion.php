ed76479a284737961275b7462404519be6ebe236
andreiito <andreortiz11@hotmail.com>
2018-04-28 11:21:48 -0500

    ADD:Correciones

--- sesion.php
+++ sesion.php
@@ -1,8 +1,9 @@
 <?php 
-
-	session_start();
+include once "conexion.php";
+	//session_start();
 	$usuario=$_POST['usuario'];
-	$pass=$_POST['password'];
+$pass=$_POST['password'];
+echo $usuario;
 
 	$validacion = $pg_query($conexion, "select * FROM usuarios WHERE usuario=".$usuario." AND contrasena=".$pass."");
 	$tipoUsuario = $pg_query($conexion, "select tipoU FROM usuarios WHERE usuario='$usuario' AND contrasena='$pass'");
@@ -10,11 +11,11 @@
 
 	if ($validacion){
 		$_SESSION['cliente']= $usuario;
-		header("Location: index.php")
+	//	header("Location: index.php")
 		echo "Bienvenido ".$usuario;
 	}
 	else{
-		header("Location: login.php");
+	//	header("Location: login.php");
 		echo "Su usuario o contraseña está incorrecto";
-	}
+	}*/
 	?>
