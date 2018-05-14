b48e62db4e8747748f077c43c316d1f6febc9231
andreiito <andreortiz11@hotmail.com>
2018-05-13 19:31:48 -0500

    ADD:ANDRE: Correcciones en consultas para s.php

--- s.php
+++ s.php
@@ -9,6 +9,9 @@ session_start();
 
 //var_dump ($_POST);
 //var_dump ($_SESSION);
-$sqlEdit= "select * from editoriales";
-var_dump($sqlEdit);
+include "conexion.php";
+$sqlEdit= "select * from editoriales;";
+$edit= consulta($sqlEdit);
+echo $edit[0]['id_editorial'];
+echo $edit[0]['nombre'];
 ?>
