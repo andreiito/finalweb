af32128abff9a8795664db72f371f635f23d683a
Andre <andreortiz11@hotmail.com>
2018-05-15 12:28:26 -0500

    Andre subir datos de comic

--- alta_articulo.php
+++ alta_articulo.php
@@ -9,7 +9,6 @@ if($_SESSION['tipo']=='admin'|| $_SESSION['tipo']=='venta'){
 	$edit = $_POST['editorial'];
 	$autor = $_POST['autor'];
 	$url = $_POST['url'];
-	
 
 	$sqlcomic="select titulo from comics WHERE titulo = '".$titulo."';";
 	$filtro_comic=consulta($sqlcomic);
@@ -18,9 +17,40 @@ if($_SESSION['tipo']=='admin'|| $_SESSION['tipo']=='venta'){
 		//echo "<p>El nombre de Usuario ya está ocupado, por favor introduzca otro</p>";
 	}
 	else{
+		if(isset($_FILES["imgcomic"]) && $_FILES["imgcomic"]["name"]){
+
 		$scomic = "insert into comics (titulo, cantidad, precio, url, id_editorial) values ('".$titulo."',".$cant.",".$precio.",'".$url."','".$edit."')";	
 		$altacomic=consulta($scomic);
-		header("Location: index.php");
+		
+	    # si es un formato de imagen
+        if($_FILES["imgcomic"]["type"]=="image/jpeg" || $_FILES["imgcomic"]["type"]=="image/pjpeg" || $_FILES["imgcomic"]["type"]=="image/gif" || $_FILES["imgcomic"]["type"]=="image/png"){
+	    # si exsite la carpeta o se ha creado
+             if(file_exists($carpetaDestino) || mkdir($carpetaDestino)){
+
+                $origen=$_FILES["imgcomic"]["tmp_name"];
+                $destino=$carpetaDestino.$_FILES["imgcomic"]["name"];
+
+		# movemos el archivo
+                if(move_uploaded_file($origen, $destino)){
+                    header("Location: index.php");
+				}
+				else{
+                        echo "<br>No se ha podido mover el archivo: ".$_FILES["imgcomic"]["name"];
+                }
+            }
+           else{
+                    echo "<br>No se ha podido crear la carpeta: ".$carpetaDestino;
+                }
+		
+        }
+        else{
+            echo "<br>".$_FILES["imgcomic"]["name"]." - NO es imagen jpg, png o gif";
+        }
+    
+    }
+    else{
+        echo "<br>No se ha subido ninguna imagen";
+    }
 	}	
 
 }
--- s.php
+++ s.php
@@ -27,35 +27,5 @@ echo $edit[0]['nombre'];*/
 #
 //echo $_FILES["imgcomic"]["tmp_name"];
 
-    if(isset($_FILES["imgcomic"]) && $_FILES["imgcomic"]["name"]){
-        # recorremos todos los arhivos que se han subido
-	    # si es un formato de imagen
-        if($_FILES["imgcomic"]["type"]=="image/jpeg" || $_FILES["imgcomic"]["type"]=="image/pjpeg" || $_FILES["imgcomic"]["type"]=="image/gif" || $_FILES["imgcomic"]["type"]=="image/png"){
-	    # si exsite la carpeta o se ha creado
-             if(file_exists($carpetaDestino) || mkdir($carpetaDestino)){
-
-                $origen=$_FILES["imgcomic"]["tmp_name"];
-                $destino=$carpetaDestino.$_FILES["imgcomic"]["name"];
-
-		# movemos el archivo
-                if(move_uploaded_file($origen, $destino)){
-                    echo "<br>".$_FILES["imgcomic"]["name"]." movido correctamente";
-				}
-				else{
-                        echo "<br>No se ha podido mover el archivo: ".$_FILES["imgcomic"]["name"];
-                }
-            }
-           else{
-                    echo "<br>No se ha podido crear la carpeta: ".$carpetaDestino;
-                }
-		
-        }
-        else{
-            echo "<br>".$_FILES["imgcomic"]["name"]." - NO es imagen jpg, png o gif";
-        }
-    
-    }
-    else{
-        echo "<br>No se ha subido ninguna imagen";
-    }
+   
 ?>
