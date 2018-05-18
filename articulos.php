3b86215f5d2c8e517bf3d9ce01765b218ae42319
andreiito <andreortiz11@hotmail.com>
2018-05-18 08:28:56 -0500

    ADD:Andre correciòn

--- articulos.php
+++ articulos.php
@@ -7,23 +7,28 @@
 <body>
 	<?php include "header.php" ?>
 	<div class="pagina">
+		<br/></br>
 		<div class="fila">
+			<h1> Articulos en venta </h1>
+		</div>
+		
 				<?php
 				include "conexion.php";
 				
 				$sqlNew= "select * from comics;";
 				$query = edit($sqlNew);
 				while ($row= pg_fetch_row($query)) {
+				echo "<div class= \"fila\">";
 					echo "<div class=\" columna_m_12 imaginario_m_12 columna_l_12 imaginario_l_12 columna_xl_12 imaginario_xl_12 articulo\">";
 					echo "<img class=\"\" alt=\"columna_m_12 imaginario_m_12 columna_l_12 imaginario_l_12 columna_xl_12 imaginario_xl_12\" src=\"".$row[4]."\">";					
 					echo "<h3 class=\"columna_m_12 imaginario_m_12 columna_l_12 imaginario_l_12 columna_xl_12 imaginario_xl_12\" >".$row[1]."</h3>";
 				  	echo "<p class=\"columna_m_12 imaginario_m_12 columna_l_12 imaginario_l_12 columna_xl_12 imaginario_xl_12\">Precio: ".$row[3]." </p>";
 				  	echo "<button id=\"addCart\">Agregar al carrito</button>";
-				  	echo "</div>";
+					echo "</div><!--fin articulo-->";
+					echo "</div><!--fin fila-->";
 			  	}
 				
 				?>
-		</div><!--Fin fila-->
 	</div><!--fin pagina-->
 
 
