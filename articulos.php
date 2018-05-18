c112df3e06567d51b376cde42b663834344ed98a
andreiito <andreortiz11@hotmail.com>
2018-05-18 08:41:28 -0500

    ADD:Andre correciòn

--- articulos.php
+++ articulos.php
@@ -20,12 +20,12 @@
 				while ($row= pg_fetch_row($query)) {
 				echo "<div class= \"fila\">";
 					echo "<div class=\" columna_m_12 imaginario_m_12 columna_l_12 imaginario_l_12 columna_xl_12 imaginario_xl_12 articulo\">";
-					echo "<h3 class=\"columna_m_12 imaginario_m_12 columna_l_12 imaginario_l_12 columna_xl_12 imaginario_xl_12\" >".$row[1]."</h3>";
-					echo "<img class=\"\" alt=\"columna_m_4 imaginario_m_4 columna_l_4 imaginario_l_4 columna_xl_4 imaginario_xl_4\" src=\"".$row[4]."\">";					
-				  	echo "<p class=\"columna_m_4 imaginario_m_4 columna_l_4 imaginario_l_4 columna_xl_4 imaginario_xl_4\">Precio: ".$row[3]." </p>";
+					echo "<h3 class=\"alinear_centro columna_m_12 columna_l_12 columna_xl_12 \" >".$row[1]."</h3>";
+					echo "<img class=\"\" alt=\"columna_m_4 columna_l_4  columna_xl_4 \" src=\"".$row[4]."\">";					
+				  	echo "<p class=\"columna_m_4 imaginario_m_4 columna_l_4 imaginario_l_4 columna_xl_4 \">Precio: ".$row[3]." </p>";
 				  	echo "<button id=\"addCart\">Agregar al carrito</button>";
 					echo "</div><!--fin articulo-->";
-					echo "</div><!--fin fila-->";
+					echo "</div><!--fin fila--><br/> <br/>";
 			  	}
 				
 				?>
